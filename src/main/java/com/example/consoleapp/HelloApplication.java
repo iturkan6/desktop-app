c673350f07ccf57470e86006fdb4505adf45839f
turkan.muradova <turkan.muradova@cybernet.az>
2023-06-20 10:51:45 +0400

    Refactoring

--- src/main/java/com/example/consoleapp/HelloApplication.java
+++ src/main/java/com/example/consoleapp/HelloApplication.java
@@ -1,14 +1,11 @@
 package com.example.consoleapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
 //  @Override
